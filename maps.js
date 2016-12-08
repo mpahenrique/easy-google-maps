127b8f0fa8633605fc24605c1aa0bc1ef41bfddc
Henrique Matheus <mpa.henrique@gmail.com>
2016-12-08 19:34:20 -0200

    First refac.

--- maps.js
+++ maps.js
@@ -1,33 +1,55 @@
-var map;
-var googleMaps = {
-    initialize: function(latitude, longitude){
-        var center = new google.maps.LatLng(latitude, longitude);
-        var mapOptions = {
-            center: center,
-            zoom: 1,
-            title: "My Local",
-            disableDefaultUI: true,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var divMapElement = document.getElementById("mapa");
-        
-        map = new google.maps.Map(divMapElement, mapOptions);
-        
-        new google.maps.Marker({
-            position: center,
-            map: map
-        });
-         
-    },
-    pin: function(latitude, longitude){
-        var position = new google.maps.LatLng(latitude, longitude);
-        if(!latitude || !longitude){
-            alert("Please, fill the fields correctly!");
-            return;
-        }
-        new google.maps.Marker({
-            position: position,
-            map: map
-        }); 
+'use strict';
+
+class GoogleMapsSample{}
+
+GoogleMapsSample.prototype.createMap = function (elmId, latitude, longitude, options){
+
+    if(!elmId || typeof latitude == 'undefined' || typeof longitude == 'undefined') { 
+        throw new Error('Missing required arguments.'); 
+        return;
     }
+
+    var mapOptions = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    mapOptions.extend(options);
+   
+    return new google.maps.Map(document.getElementById(elmId), mapOptions);
+}
+
+GoogleMapsSample.prototype.pin = function (map, latitude, longitude, options){
+
+    if(!map || !latitude || !longitude) { 
+        throw new Error('Missing required arguments.'); 
+        return;
+    }
+
+    var position = new google.maps.LatLng(latitude, longitude)
+    ,   markerOptions = {
+        position: position,
+        map: this.map
+    };
+    markerOptions.extend(options);
+
+    return new google.maps.Marker(markerOptions);
+}
+
+/*
+    GoogleMapsSample.prototype.customMethod = function(map){
+        // your custom method here
+    }
+*/
+
+Object.prototype.extend = function(obj){
+    if(typeof obj !== 'object') return this;
+    for (var attrname in obj) { this[attrname] = obj[attrname]; }
+    return this;
+}
+
+if(this.module && this.module.exports){
+    module.exports = new GoogleMapsSample();
+} else {
+    window.googleMapsSample = new GoogleMapsSample();
 }
